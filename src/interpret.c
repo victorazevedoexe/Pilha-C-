5c1cd795cb08e4fa93039bcb9582bb5646236aad
victorazevedoexe <jvlazevedo03@gmail.com>
2024-12-16 15:32:37 -0300

    Update interpret.c

--- src/interpret.c
+++ src/interpret.c
@@ -2,52 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "interpret.h"
-#include "stack.h"
 
-static Stack* stack = NULL;
-
-void interpret(const char* source) {
-    if (!stack) {
-        stack = new_stack(100);  // Tamanho máximo da pilha
-    }
-
+void interpret(const char *source) {
     char op[10];
-    int arg;
-
-    if (sscanf(source, "%s %d", op, &arg) == 2) {
-        if (strcmp(op, "push") == 0) {
-            stack_push(stack, arg);
-        } else {
-            printf("Comando desconhecido: %s\n", op);
-        }
-    } else if (sscanf(source, "%s", op) == 1) {
-        if (strcmp(op, "add") == 0) {
-            int b = stack_pop(stack);
-            int a = stack_pop(stack);
-            stack_push(stack, a + b);
-        } else if (strcmp(op, "sub") == 0) {
-            int b = stack_pop(stack);
-            int a = stack_pop(stack);
-            stack_push(stack, a - b);
-        } else if (strcmp(op, "mul") == 0) {
-            int b = stack_pop(stack);
-            int a = stack_pop(stack);
-            stack_push(stack, a * b);
-        } else if (strcmp(op, "div") == 0) {
-            int b = stack_pop(stack);
-            if (b == 0) {
-                printf("Erro: Divisão por zero!\n");
-                stack_push(stack, b); // Retorna b para a pilha
-                return;
-            }
-            int a = stack_pop(stack);
-            stack_push(stack, a / b);
-        } else if (strcmp(op, "print") == 0) {
-            stack_print(stack);
-        } else {
-            printf("Comando desconhecido: %s\n", op);
-        }
-    } else {
-        printf("Erro: Comando inválido!\n");
-    }
+    char arg[10];
+    sscanf(source, "%s%s", op, arg);
+    printf("Operação: %s\n", op);
+    printf("Argumento: %s\n", arg);
 }
