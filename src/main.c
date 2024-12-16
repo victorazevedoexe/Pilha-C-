dd9c5c697609d85e063751c2beb87e839c87bc91
victorazevedoexe <jvlazevedo03@gmail.com>
2024-12-16 14:09:01 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include "interpret.h"
+
+static void repl() {
+    char line[1024];
+    for (;;) {
+        printf("> ");
+        if (!fgets(line, sizeof(line), stdin)) {
+            printf("\n");
+            break;
+        }
+        interpret(line);
+    }
+}
+
+int main() {
+    printf("Bem-vindo ao interpretador de máquina baseada em pilha.\n");
+    printf("Comandos disponíveis: push <valor>, add, sub, mul, div, print, exit\n");
+    repl();
+    return 0;
+}
 
